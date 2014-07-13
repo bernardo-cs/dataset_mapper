7c9e8687b5720e88da2a9c30aaa663c84345e36b
Bernardo <bersimoes@gmail.com>
2014-07-13 19:33:04 +0100

    fixed a bug where no tweet is found

--- lib/dataset_mapper/tweets_json_connection.rb
+++ lib/dataset_mapper/tweets_json_connection.rb
@@ -3,12 +3,14 @@ module DatasetMapper
   ## and tweets module. Enables reading of the original text in tweets
 
   module TweetsJsonConnection
+    ## if no tweet is detected, returns the received text
     def get_id_from_stemmed_text text
-      tweets_with_id.detect{ |t| t.last == text }.first
+      detected_tweet = tweets_with_id.detect{ |t| t.last == text }
+      detected_tweet.nil? ? text  : detected_tweet.first
     end
 
     def convert_stemmed_text_to_text text
-      get_text_from(get_id_from_stemmed_text( text ))
+      get_text_from( get_id_from_stemmed_text( text ))
     end
   end
 end
